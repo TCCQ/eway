72c3954c7ed9ce7b4c2a61bc200864dc2ada29b9
tmu <t.man.atl.2012@gmail.com>
2023-01-03 08:55:22 -0700

    Keyboard input. still broken

--- comp/comp.c
+++ comp/comp.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <getopt.h>
+#include <stdbool.h>
 #include <assert.h>
 #include <stdio.h>
 
@@ -15,7 +16,11 @@
 #include <wlr/types/wlr_output_layout.h>
 #include <wlr/types/wlr_scene.h>
 #include <wlr/types/wlr_xdg_shell.h>
+#include <wlr/types/wlr_input_device.h>
+#include <wlr/types/wlr_seat.h>
 #include <wlr/util/log.h>
+
+#include <xkbcommon/xkbcommon.h>
 
 struct server {
   struct wl_display *display;	/* talk with the comp */
@@ -32,7 +37,15 @@ struct server {
   struct wl_listener new_xdg_surface;
   struct wl_list views;
 
-  /* input stuff goes here */
+  struct wlr_seat *seat;
+  struct wl_listener new_input;
+  struct wl_list keyboards;
+  /*
+   * struct wl_listener request_cursor;
+   * struct wl_listener request_set_selection;
+   */
+
+
   
   uint32_t surface_offset; 	/* (?) */
 
@@ -76,6 +89,20 @@ struct view {
   int x, y;
 };
 
+struct keyboard {
+  struct wl_list link;
+  struct server *server;
+  struct wlr_keyboard *wlr_keyboard;
+
+  struct wl_listener modifiers;
+  struct wl_listener key;
+  struct wl_listener destroy;
+};
+
+/*
+ * new outputs, and other server handle_ type stuff
+ */
+/* {{{ */
 
 static void output_handle_frame (struct wl_listener *listener, void *data) {
   struct output *output = wl_container_of(listener, output, frame);
@@ -158,7 +185,12 @@ static void server_handle_new_surface(struct wl_listener *listener, void *data) 
   wlr_scene_node_set_position(&surface->scene_surface->buffer->node, pos, pos);
 }
 
-/* now we need xdg stuff. this will be mostly stolen from  tinywl */
+/* }}} */
+
+/*
+ * xdg_shell stuff. mostly stolen from tinywl
+ */
+/* {{{ */
 
 static void xdg_toplevel_map (struct wl_listener *listener, void *data) {
   /* surface is mapped (ready to display) */
@@ -217,6 +249,165 @@ static void server_new_xdg_surface (struct wl_listener *listener, void *data) {
   wl_signal_add(&xdg_surface->events.destroy, &view->destroy);
 }
 
+/* }}} */
+
+/*
+ * input stuff. stolen from tinywl
+ */
+
+static void keyboard_focus_to_view (struct view *view, struct wlr_surface *surface) {
+  if (!view) {
+    return;
+  }
+  struct server *server = view->server;
+  struct wlr_seat *seat = server->seat;
+  struct wlr_surface *prev_surface = seat->keyboard_state.focused_surface;
+  if (prev_surface == surface) {
+    /* dont' refocus */
+    return;
+  }
+
+  if (prev_surface) {
+    /* remove focus from prev */
+    struct wlr_xdg_surface *previous = wlr_xdg_surface_from_wlr_surface(seat->keyboard_state.focused_surface);
+    assert(previous->role == WLR_XDG_SURFACE_ROLE_TOPLEVEL);
+    wlr_xdg_toplevel_set_activated(previous->toplevel, false);
+  }
+  struct wlr_keyboard *keyboard = wlr_seat_get_keyboard(seat);
+  /* raise view */
+  wlr_scene_node_raise_to_top(&view->scene_tree->node);
+  wl_list_remove(&view->link);
+  wl_list_insert(&server->views, &view->link);
+  /* not really sure what is happening here ^ */
+  wlr_xdg_toplevel_set_activated(view->xdg_toplevel, true);
+  /* wlroots will automatically forward key inputs elsewhere */
+  if (keyboard) {
+    wlr_seat_keyboard_notify_enter(seat, view->xdg_toplevel->base->surface,
+				   keyboard->keycodes, keyboard->num_keycodes, &keyboard->modifiers);
+  }
+}
+
+static void keyboard_handle_modifiers (struct wl_listener *listener, void *data) {
+  /* raised when any modifier is pressed */
+  struct keyboard *keyboard = wl_container_of(listener, keyboard, modifiers);
+  /* workaround for seats only allowing one keyboard */
+  wlr_seat_set_keyboard(keyboard->server->seat, keyboard->wlr_keyboard);
+  /* send to client */
+  wlr_seat_keyboard_notify_modifiers(keyboard->server->seat, &keyboard->wlr_keyboard->modifiers);
+}
+
+static bool handle_keybinding (struct server *server, uint32_t modifiers, xkb_keysym_t sym) {
+  /* should the compositor consume this keystroke? if so, do the action */
+  if (modifiers & WLR_MODIFIER_ALT) {
+    switch (sym) {
+    case XKB_KEY_Escape:
+      wl_display_terminate(server->display);
+      return true;
+    case XKB_KEY_F1:
+      /* Cycle to the next view */
+      if (!wl_list_length(&server->views) < 2) {
+	struct view *next_view = wl_container_of(server->views.prev, next_view, link);
+	keyboard_focus_to_view(next_view, next_view->xdg_toplevel->base->surface);
+      }
+      return true;
+    default:
+      return false;
+    }
+  }
+  return false;
+}
+
+static void keyboard_handle_key (struct wl_listener *listener, void *data) {
+  /* raised when key is pressed or released */
+  struct keyboard *keyboard = wl_container_of(listener, keyboard, key);
+  struct server *server = keyboard->server;
+  struct wlr_keyboard_key_event *event = data;
+  struct wlr_seat *seat = server->seat;
+
+  /* libinput -> xkbcommon */
+  uint32_t keycode = event->keycode + 8;
+  /* get keysyms based on keymap */
+  const xkb_keysym_t *syms;
+  int nsyms = xkb_state_key_get_syms(keyboard->wlr_keyboard->xkb_state, keycode, &syms);
+  uint32_t modifiers = wlr_keyboard_get_modifiers(keyboard->wlr_keyboard);
+  bool handled = false;
+  
+  if (event->state == WL_KEYBOARD_KEY_STATE_PRESSED) {
+    /* only trigger on downstroke */
+    for (int i = 0; i < nsyms; i++) {
+      handled = handle_keybinding(server, modifiers, syms[i]);
+    }
+  }
+  /* I don't think I understand how `handled` works here, so for now this follows what tinywl does (mostly) */
+  if (!handled) {
+    /* pass to client */
+    wlr_seat_set_keyboard(seat, keyboard->wlr_keyboard);
+    wlr_seat_keyboard_notify_key(seat, event->time_msec, event->keycode, event->state);
+  }
+}
+
+static void keyboard_handle_destory (struct wl_listener *listener, void *data) {
+  struct keyboard *keyboard = wl_container_of(listener, keyboard, destroy);
+  wl_list_remove(&keyboard->modifiers.link);
+  wl_list_remove(&keyboard->key.link);
+  wl_list_remove(&keyboard->destroy.link);
+  wl_list_remove(&keyboard->link);
+  free(keyboard);
+}
+
+static void server_new_keyboard (struct server *server, struct wlr_input_device *device) {
+  struct wlr_keyboard *wlr_keyboard = wlr_keyboard_from_input_device(device);
+  struct keyboard *keyboard = calloc(1, sizeof(struct keyboard));
+  keyboard->server = server;
+  keyboard->wlr_keyboard = wlr_keyboard;
+
+  /* pick and assign keymap. assumes default */
+  struct xkb_context *context = xkb_context_new(XKB_CONTEXT_NO_FLAGS);
+  struct xkb_keymap *keymap = xkb_keymap_new_from_names(context, NULL, XKB_KEYMAP_COMPILE_NO_FLAGS);
+
+  wlr_keyboard_set_keymap(wlr_keyboard, keymap);
+  xkb_keymap_unref(keymap);
+  xkb_context_unref(context);
+  wlr_keyboard_set_repeat_info(wlr_keyboard, 25, 600);
+  /* parameterize ^ */
+
+  /* set listeners */
+  keyboard->modifiers.notify = keyboard_handle_modifiers;
+  wl_signal_add(&wlr_keyboard->events.modifiers, &keyboard->modifiers);
+  keyboard->key.notify = keyboard_handle_key;
+  wl_signal_add(&wlr_keyboard->events.key, &keyboard->key);
+  keyboard->destroy.notify = keyboard_handle_destory;
+  wl_signal_add(&device->events.destroy, &keyboard->destroy);
+
+  wlr_seat_set_keyboard(server->seat, keyboard->wlr_keyboard);
+
+  wl_list_insert(&server->keyboards, &keyboard->link);
+}
+
+static void server_new_input (struct wl_listener *listener, void *data) {
+  /* raised on new input device */
+  struct server *server = wl_container_of(listener, server, new_input);
+  struct wlr_input_device *device = data;
+  switch (device->type) {
+  case WLR_INPUT_DEVICE_KEYBOARD:
+    server_new_keyboard(server, device);
+    break;
+  case WLR_INPUT_DEVICE_POINTER:
+    /* later */
+    break;
+  default:
+    break;
+  }
+  /* let the wlr_seat our capabilities, which is communiciated
+   * to the client. we will always have a cursor, even if there
+   * are no pointer devices, so start with that. */
+  uint32_t caps = WL_SEAT_CAPABILITY_POINTER;
+  if (!wl_list_empty(&server->keyboards)) {
+    caps |= WL_SEAT_CAPABILITY_KEYBOARD;
+  }
+  wlr_seat_set_capabilities(server->seat, caps);
+}
+
 int main(int argc, char** argv) {
   wlr_log_init(WLR_DEBUG, NULL);
 
@@ -258,9 +449,26 @@ int main(int argc, char** argv) {
   wlr_scene_attach_output_layout(server.scene, server.output_layout);
 
   wl_list_init(&server.views);
-  server.xdg_shell = wlr_xdg_shell_create(server.display, 3); /* ? */
+  server.xdg_shell = wlr_xdg_shell_create(server.display, 3); 
   server.new_xdg_surface.notify = server_new_xdg_surface;
   wl_signal_add(&server.xdg_shell->events.new_surface, &server.new_xdg_surface);  
+
+  /* add cursor stuff here */
+  
+  /* configure a single seat, which contains all input devices */  
+  wl_list_init(&server.keyboards);
+  server.new_input.notify = server_new_input;
+  wl_signal_add(&server.backend->events.new_input, &server.new_input);
+  server.seat = wlr_seat_create(server.display, "seat0");
+  /*
+   * server.request_cursor.notify = seat_request_cursor;
+   * wl_signal_add(&server.seat->events.request_set_cursor,
+   * 		&server.request_cursor);
+   * server.request_set_selection.notify = seat_request_set_selection;
+   * wl_signal_add(&server.seat->events.request_set_selection,
+   * 		&server.request_set_selection);
+   */
+
   
   const char *socket = wl_display_add_socket_auto(server.display);
   if (!socket) {
